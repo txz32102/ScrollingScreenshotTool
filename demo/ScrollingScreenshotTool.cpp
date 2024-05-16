34ac90b9099d1b25b69e40670b3d70d13ef63638
musong <txz32102@gmail.com>
2024-05-16 12:52:16 +0800

    given area screenshot

--- demo/ScrollingScreenshotTool.cpp
+++ demo/ScrollingScreenshotTool.cpp
@@ -3,18 +3,27 @@
 #include <iostream>
 #include <string>
 
-#pragma comment (lib,"Gdiplus.lib")
-#pragma comment (lib,"Gdi32.lib")
-#pragma comment (lib,"User32.lib")
-#pragma comment (lib,"Ole32.lib")
+#pragma comment(lib, "Gdiplus.lib")
+#pragma comment(lib, "Gdi32.lib")
+#pragma comment(lib, "User32.lib")
+#pragma comment(lib, "Ole32.lib")
 
 using namespace Gdiplus;
 
+// Custom Point structure to avoid conflict with Gdiplus::Point
+struct MyPoint {
+    int x;
+    int y;
+};
+
+// Global variables
 const wchar_t g_szClassName[] = L"myWindowClass";
 ULONG_PTR gdiplusToken;
-HWND hEdit;  // 输入框句柄
+MyPoint startPoint, endPoint;
+bool capturing = false;
+HWND hEdit;  // Input box handle
 
-// 初始化 GDI+
+// Initialize GDI+
 void InitGDIPlus() {
     GdiplusStartupInput gdiplusStartupInput;
     Status status = GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
@@ -24,39 +33,35 @@ void InitGDIPlus() {
     }
 }
 
-// 关闭 GDI+
+// Shutdown GDI+
 void ShutdownGDIPlus() {
     GdiplusShutdown(gdiplusToken);
 }
 
-// 获取编码器 CLSID
+// Get encoder CLSID
 int GetEncoderClsid(const WCHAR* format, CLSID* pClsid) {
-    UINT num = 0;          // 编码器数量
-    UINT size = 0;         // 存储编码器信息的大小
-
+    UINT num = 0;
+    UINT size = 0;
     GetImageEncodersSize(&num, &size);
-    if (size == 0) return -1;  // 没有编码器
+    if (size == 0) return -1;
 
     ImageCodecInfo* pImageCodecInfo = (ImageCodecInfo*)(malloc(size));
-    if (pImageCodecInfo == NULL) return -1;  // 内存分配失败
+    if (pImageCodecInfo == NULL) return -1;
 
     GetImageEncoders(num, size, pImageCodecInfo);
-
     for (UINT j = 0; j < num; ++j) {
         if (wcscmp(pImageCodecInfo[j].MimeType, format) == 0) {
             *pClsid = pImageCodecInfo[j].Clsid;
             free(pImageCodecInfo);
-            return j;  // 成功返回下标
+            return j;
         }
     }
-
     free(pImageCodecInfo);
-    return -1;  // 没有找到
+    return -1;
 }
 
-// 保存截图的函数
+// Save bitmap to file
 void SaveBitmapToFile(HBITMAP hBitmap, const WCHAR* filePath) {
-    // 从 HBITMAP 创建 GDI+ Bitmap
     Bitmap bitmap(hBitmap, nullptr);
     if (bitmap.GetLastStatus() != Ok) {
         std::wcerr << L"Failed to create GDI+ Bitmap" << std::endl;
@@ -69,7 +74,6 @@ void SaveBitmapToFile(HBITMAP hBitmap, const WCHAR* filePath) {
         return;
     }
 
-    // 保存到文件
     Status status = bitmap.Save(filePath, &clsid, nullptr);
     if (status != Ok) {
         std::wcerr << L"Failed to save bitmap to file" << std::endl;
@@ -79,100 +83,97 @@ void SaveBitmapToFile(HBITMAP hBitmap, const WCHAR* filePath) {
     }
 }
 
-// 截屏函数
+// Capture screenshot
 void CaptureScreenshot() {
-    // 获取屏幕尺寸
-    int screenWidth = GetSystemMetrics(SM_CXSCREEN);
-    int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+    int width = endPoint.x - startPoint.x;
+    int height = endPoint.y - startPoint.y;
 
-    // 创建整个屏幕的设备上下文
-    HDC hScreenDC = GetDC(nullptr);
-    HDC hMemoryDC = CreateCompatibleDC(hScreenDC);
+    HDC hdcScreen = GetDC(NULL);
+    HDC hdcMemDC = CreateCompatibleDC(hdcScreen);
+    HBITMAP hBitmap = CreateCompatibleBitmap(hdcScreen, width, height);
+    SelectObject(hdcMemDC, hBitmap);
+    BitBlt(hdcMemDC, 0, 0, width, height, hdcScreen, startPoint.x, startPoint.y, SRCCOPY);
 
-    // 创建一个与屏幕设备上下文兼容的位图
-    HBITMAP hBitmap = CreateCompatibleBitmap(hScreenDC, screenWidth, screenHeight);
-    if (!hBitmap) {
-        std::wcerr << L"Failed to create compatible bitmap" << std::endl;
-        DeleteDC(hMemoryDC);
-        ReleaseDC(nullptr, hScreenDC);
-        return;
-    }
-
-    // 将新位图选择到内存设备上下文中
-    HBITMAP hOldBitmap = (HBITMAP)SelectObject(hMemoryDC, hBitmap);
-
-    // 进行位块传输到我们的兼容内存 DC 中
-    BitBlt(hMemoryDC, 0, 0, screenWidth, screenHeight, hScreenDC, 0, 0, SRCCOPY);
-
-    // 恢复旧位图
-    SelectObject(hMemoryDC, hOldBitmap);
-
-    // 保存位图到文件
     SaveBitmapToFile(hBitmap, L"screenshot.png");
 
-    // 清理
     DeleteObject(hBitmap);
-    DeleteDC(hMemoryDC);
-    ReleaseDC(nullptr, hScreenDC);
+    DeleteDC(hdcMemDC);
+    ReleaseDC(NULL, hdcScreen);
 }
 
-// 延迟指定秒数
-void Delay(int seconds) {
-    Sleep(seconds * 1000);  // 将秒数转换为毫秒
+// Mouse hook procedure
+LRESULT CALLBACK LowLevelMouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode == HC_ACTION) {
+        MSLLHOOKSTRUCT* mouseStruct = (MSLLHOOKSTRUCT*)lParam;
+
+        if (wParam == WM_LBUTTONDOWN) {
+            startPoint.x = mouseStruct->pt.x;
+            startPoint.y = mouseStruct->pt.y;
+            capturing = true;
+            std::cout << "Start Point: (" << startPoint.x << ", " << startPoint.y << ")\n";
+        }
+        else if (wParam == WM_LBUTTONUP && capturing) {
+            endPoint.x = mouseStruct->pt.x;
+            endPoint.y = mouseStruct->pt.y;
+            capturing = false;
+            std::cout << "End Point: (" << endPoint.x << ", " << endPoint.y << ")\n";
+            PostQuitMessage(0); // Exit message loop
+        }
+    }
+    return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch (msg)
-    {
-    case WM_CREATE:
-    {
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+    case WM_CREATE: {
         CreateWindow(
-            L"BUTTON",  // 预定义类；假定为 Unicode 
-            L"Click Me",      // 按钮文本 
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // 样式 
-            100,         // x 位置 
-            100,         // y 位置 
-            100,        // 按钮宽度
-            30,        // 按钮高度
-            hwnd,       // 父窗口
-            (HMENU)1,       // 无菜单。
+            L"BUTTON",
+            L"Click Me",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            100,
+            100,
+            100,
+            30,
+            hwnd,
+            (HMENU)1,
             (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
-            NULL);      // 无需指针。
+            NULL);
 
-        // 创建输入框
+        // Create input box
         hEdit = CreateWindow(
-            L"EDIT",   // 预定义类
-            L"",       // 初始文本
-            WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER, // 样式
-            100, 50, 100, 25,   // 位置和大小
+            L"EDIT",
+            L"",
+            WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER,
+            100, 50, 100, 25,
             hwnd, NULL, (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), NULL);
     }
-    break;
+                  break;
     case WM_COMMAND:
-        if (LOWORD(wParam) == 1)
-        {
-            // 最小化窗口
+        if (LOWORD(wParam) == 1) {
             ShowWindow(hwnd, SW_MINIMIZE);
 
-            // 获取用户输入的延迟时间
             wchar_t buffer[10];
             GetWindowText(hEdit, buffer, 10);
-            int delaySeconds = _wtoi(buffer);  // 将文本转换为整数
+            int delaySeconds = _wtoi(buffer);
 
-            // 延迟指定的秒数
-            Delay(delaySeconds);
+            Sleep(delaySeconds * 1000);
 
-            // 截屏
+            // Set mouse hook
+            HHOOK mouseHook = SetWindowsHookEx(WH_MOUSE_LL, LowLevelMouseProc, NULL, 0);
+            std::cout << "Move the mouse to the start point and click...\n";
+            MSG msg;
+            while (GetMessage(&msg, NULL, 0, 0)) {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+            UnhookWindowsHookEx(mouseHook);
+
             CaptureScreenshot();
-            MessageBox(hwnd, L"Screenshot saved as screenshot.bmp", L"Message", MB_OK);
+            MessageBox(hwnd, L"Screenshot saved as screenshot.png", L"Message", MB_OK);
         }
         break;
-
     case WM_CLOSE:
-        // 在这里处理 WM_CLOSE 消息以防止窗口关闭
-        if (MessageBox(hwnd, L"Are you sure you want to close?", L"Close", MB_OKCANCEL) == IDOK)
-        {
+        if (MessageBox(hwnd, L"Are you sure you want to close?", L"Close", MB_OKCANCEL) == IDOK) {
             DestroyWindow(hwnd);
         }
         break;
@@ -185,8 +186,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     InitGDIPlus();
 
     WNDCLASSEX wc;
@@ -206,8 +206,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpszClassName = g_szClassName;
     wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
-    if (!RegisterClassEx(&wc))
-    {
+    if (!RegisterClassEx(&wc)) {
         MessageBox(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
         ShutdownGDIPlus();
         return 0;
@@ -221,8 +220,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         CW_USEDEFAULT, CW_USEDEFAULT, 540, 200,
         NULL, NULL, hInstance, NULL);
 
-    if (hwnd == NULL)
-    {
+    if (hwnd == NULL) {
         MessageBox(NULL, L"Window Creation Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
         ShutdownGDIPlus();
         return 0;
@@ -231,8 +229,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-    while (GetMessage(&Msg, NULL, 0, 0) > 0)
-    {
+    while (GetMessage(&Msg, NULL, 0, 0) > 0) {
         TranslateMessage(&Msg);
         DispatchMessage(&Msg);
     }
